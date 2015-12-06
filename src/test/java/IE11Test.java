7fe1af8199191a80c1403fe10b5c07060f5642af
Szymon Kazmierczak <szymonkaz@gmail.com>
2015-12-06 23:39:44 +0000

    Added a test for ie11

---
+++ src/test/java/IE11Test.java
@@ -0,0 +1,41 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by szymonk on 12/6/15.
+ */
+public class IE11Test {
+
+    private String SAUCE_USERNAME = System.getenv("SAUCE_USERNAME");
+    private String SAUCE_KEY = System.getenv("SAUCE_KEY");
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+        capabilities.setCapability("version", "11");
+        capabilities.setCapability("platform", Platform.WIN8_1);
+        this.driver = new RemoteWebDriver(new URL(SAUCE_USERNAME + ":" + SAUCE_KEY + "@ondemand.saucelabs.com:80/wd/hub")
+                ,capabilities);
+    }
+
+    @Test
+    public void webDriver() throws Exception {
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
