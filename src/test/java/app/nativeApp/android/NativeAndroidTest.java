9243a613d511fe1df08f97fbf1c115421e853965
Szymon Kazmierczak <szymonkaz@gmail.com>
2015-12-19 20:49:57 +0000

    cleanup of NativeAndroidTest
    some cleanup + added name of the build

--- src/test/java/app/nativeApp/android/NativeAndroidTest.java
+++ src/test/java/app/nativeApp/android/NativeAndroidTest.java
@@ -36,19 +36,20 @@ public class NativeAndroidTest {
 
         DesiredCapabilities capabilities = DesiredCapabilities.android();
         capabilities.setCapability("build","Native Android Test Suite");
+        capabilities.setCapability("name", "Samsung Galaxy S4");
 
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Samsung Galaxy S4 Emulator");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.4");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.APP, "sauce-storage:ApiDemos-debug.apk");
-        capabilities.setCapability("browserName", "");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
         capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "io.appium.android.apis");
         capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "ApiDemos");
         capabilities.setCapability(MobileCapabilityType.APP_WAIT_ACTIVITY, "ApiDemos");
-        capabilities.setCapability("deviceReadyTimeout", 40);
-        capabilities.setCapability("newCommandTimeout", 180);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_READY_TIMEOUT, 40);
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 180);
+        capabilities.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.4.16");
         capabilities.setCapability("deviceOrientation", "portrait");
-        capabilities.setCapability("appiumVersion", "1.4.16");
 
         driver = new AndroidDriver(new URL("http://" + SAUCE_USERNAME + ":" + SAUCE_KEY + "@ondemand.saucelabs.com:80/wd/hub")
                 ,capabilities);
